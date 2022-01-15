0884ad581334129b27e23dee3f056d354b907bdc
Daniel Lopes <daniel.dxdb@gmail.com>
2022-01-15 17:05:49 -0300

    Finishing menu

--- Program.cs
+++ Program.cs
@@ -12,10 +12,19 @@ namespace StopWatch
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Seconds");
-            Console.WriteLine("M = Minutes");
+            Console.WriteLine("S = Seconds -> 10s = 10 seconds");
+            Console.WriteLine("M = Minutes -> 1m = 1 minute");
             Console.WriteLine("0 = Exit");
             Console.WriteLine("How long do you want to count?");
+
+            string input = Console.ReadLine().ToLower();
+            if (input == "0")
+            {
+                return;
+            }
+            char type = char.Parse(input.Substring(input.Length - 1, 1));
+            int time = int.Parse(input.Substring(0, input.Length - 1));
+            Start(type == 's' ? time : time * 60);
         }
         static void Start(int time = 10)
         {
@@ -31,6 +40,7 @@ namespace StopWatch
             Console.Clear();
             Console.WriteLine("Count completed...");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
